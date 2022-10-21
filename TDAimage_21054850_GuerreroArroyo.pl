8cfec59ebc38aa74a0547a556ebea66bb1cb5ec2
BastianGuerreroA <bastian.guerrero.a@usach.cl>
2022-10-20 22:26:18 -0300

    Update TDAimage_21054850_GuerreroArroyo.pl
    Se agrega el constructor de la imagen (el cual esta representación de la imagen se ocupara durante todo el desarrollo del laboratorio ) , como ademas los predicados : imageIsBitmmap , imageIsPixmap ,imageIsHexmap.

--- TDAimage_21054850_GuerreroArroyo.pl
+++ TDAimage_21054850_GuerreroArroyo.pl
@@ -46,3 +46,74 @@ pixrgb_d(Alto,Ancho,R,G,B,D,[Alto,Ancho,R,G,B,D]):-
     G >= 0 ,  G =< 255,
     B >= 0 ,  B =< 255,
     D >= 0 .
+
+%Constructor de la lista imagen
+image(Ancho , Alto , [C|L] , I):-
+    number(Ancho),
+    number(Alto),
+    append([C],L,X),
+    I = [Ancho , Alto , X].
+
+%-------------------------------------------------
+
+obtposicion(1,[X|_],X):- !.
+obtposicion(N,[_|L],X):-
+    N1 is N-1,
+    obtposicion(N1,L,X).
+
+contador([],0):- !.
+contador([_|L],C):- contador(L,M), C is M+1.
+
+%---------------------------------------------------
+
+bit(X):-
+    number(X),
+    X = 0, ! ; X = 1.
+
+pixbit(P):-
+    contador(P , C),
+    C == 4,
+    obtposicion(3,P,X),
+    bit(X).
+
+pixelesbit([]).
+pixelesbit([X|Y]):-
+    pixbit(X),
+    pixelesbit(Y).
+
+%imageIsBitmmap
+
+imageIsBitmap(I):-
+    pixeles(I,P),
+    pixelesbit(P).
+%-------------------------------------------------------
+
+pixRGB(P):-
+    contador(P, C),
+    C == 6.
+
+pixelesRGB([]).
+pixelesRGB([X|Y]):-
+    pixRGB(X),
+    pixelesRGB(Y).
+
+imageIsPixmap(I):-
+    pixeles(I,P),
+    pixelesRGB(P).
+
+
+%-------------------------------------------------------------
+pixhex(P):-
+    contador(P, C),
+    C == 4,
+    obtposicion(3,P,X),
+    string(X).
+
+pixelesHex([]).
+pixelesHex([X|Y]):-
+    pixhex(X),
+    pixelesHex(Y).
+
+imageIsHexmap(I):-
+    pixeles(I,P),
+    pixelesHex(P).
